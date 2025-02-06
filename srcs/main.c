d46e2ac0c16387b91f1a47c9b2992dbd261ed341
Arilson Baldelomar Carballo <abaldelo@student.42madrid.com>
2025-02-06 21:48:26 +0100

    main complete

--- srcs/main.c
+++ srcs/main.c
@@ -6,31 +6,45 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:20:37 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/02/04 18:43:48 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/02/06 18:51:31 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include <stdio.h>
 
+void free_matriz(char **matriz)
+{
+	int i;
+	i = 0;
+	while(matriz[i])
+	{
+		free(matriz[i]);
+		i++;
+	}
+	free(matriz);
+}
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
-	// t_stack	*stack_b;
+	char *str;
 
-	if (!validate_input(argc, argv))
+	if(argc == 2)
 	{
-		printf("Error\n");
-		return (1);
+		str = ft_strjoin("ignore ", argv[1]);
+		argv = ft_split(str, ' ');
+		free(str);
+		argc = 0;
+		while(argv[argc])
+			argc++;
 	}
+	if (!validate_input(argc, argv))
+		return (ft_putendl_fd("Error",2),EXIT_FAILURE);
     stack_a = init_stack(argc, argv);
-	print_stack(stack_a);
+	free_matriz(argv);
 	if (stack_a->size <= 5)
 		sort_small(stack_a);
-	print_stack(stack_a);
-	
-    // stack_b = malloc(sizeof(t_stack));
-    // stack_b->top = NULL;
-    // stack_b->size = 0;
-
-	return (0);
+	else
+		sort_large(stack_a);
+	return (EXIT_SUCCESS);
 }
