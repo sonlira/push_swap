3e9a21c8c9b97b2b9b06b8911763c03a32b60e3d
Ari <aril.abcmail@gmail.com>
2025-02-09 22:11:19 +0100

    is_an_ordered_list added and structure s_node modified

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 12:18:58 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/02/08 23:49:12 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/02/09 21:55:37 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@
 typedef struct s_node
 {
 	int				value;
+	int				index;
 	struct s_node	*next;
+	struct s_node	*prev;
 }	t_node;
 
 typedef struct s_stack
@@ -35,10 +37,11 @@ int		validate_input(int argc, char **argv);
 int		is_valid_number(char *str);
 long	ft_atol(const char *str);
 int		has_duplicates(char **argv);
+int		is_an_ordered_list(t_stack *stack);
 
 t_stack	*init_stack(int argc, char **argv);
 
-t_node	*new_node(int value);
+t_node	*new_node(int value, int index);
 void	free_stack(t_stack **stack);
 int		find_min(t_stack *stack);
 int		find_min_position(t_stack *stack, int min_value);
@@ -51,8 +54,5 @@ void	reverse_rotate(t_stack *stack, char *operator);
 
 void	sort_small(t_stack *stack);
 void	sort_large(t_stack *stack);
-void	move_chunks_to_b(t_stack *s_a, t_stack *s_b, int *chunks, int n_chun);
-void	bubble_sort(int *values, int size);
-int		get_num_chunks(int size);
 
 #endif
